1417fa235189186882c9022c9cb88194154b0d56
Fazal0567 <fazalalicontribute@gmail.com>
2025-06-20 04:27:26 +0530

    sdden new pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
-import { Route,Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Blog from './pages/Blog'
+import Dashboard from './pages/admin/Dashboard'
+import Layout from './pages/admin/Layout'
+import AddBlog from './pages/admin/AddBlog'
+import ListBlog from './pages/admin/ListBlog'
+import Comments from './pages/admin/Comments'
+import Login from './components/admin/Login'
 
 const App = () => {
   return (
     <div>
       <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/blog/:id" element={<Blog/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/blog/:id" element={<Blog />} />
+        <Route path="/admin" element={true ? <Layout /> : <Login />}>
+          <Route index element={<Dashboard />} />
+          <Route path="addblog" element={<AddBlog />} />
+          <Route path="listblog" element={<ListBlog />} />
+          <Route path="comments" element={<Comments />} />
+        </Route>
       </Routes>
     </div>
   )
---
+++ client/src/pages/admin/ListBlog.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ListBlog = () => {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default ListBlog
